a105fd1d48456c68f2953c16e50b5a7f457a5f69
Hamdi Kandil <hamdikendil.insatgmail.com>
2026-03-04 15:08:58 +0100

    trivial edit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// This is the main entry point of the application. It exports a function that can be used in other parts of the application.
 // This is a simple function that takes a name as an argument and returns a greeting message.
 function sayHi(name) {
   return `Hello ${name}`
